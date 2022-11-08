2839dc405f612c9a9e6095d338b9db2d5ae9e943
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-11-07 18:53:46 -0700

    commit tree ac884c5dc380b3a4dcc090a8c1a69c1e497d4382

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 const user = {
   name: "Duane",
-  interests: ["Coding", "Biking", "Words ending in 'ing'"],
+  interests: ["Coding", "Climbing", "Words ending in 'ing'"],
 };
 
 export default user;
